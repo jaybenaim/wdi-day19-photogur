6f2b44b2eafc1c69f5582316e8a63ce517ff1c9b
jaybenaim <benaimjacob@gmail.com>
2019-07-31 12:22:52 -0400

    added stretch search by any

--- photogur/views.py
+++ photogur/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse, HttpResponseRedirect 
 from django.shortcuts import render
 from photogur.models import * 
+from django.contrib.postgres.search import SearchVector
 
 
 
@@ -28,8 +29,8 @@ def picture_show(request, id):
     
 def picture_search(request): 
     query = request.GET['query']
-    search_results = Picture.objects.filter(artist=query)
-    context = {'picture': search_results, 'query': query }
+    search_results = Picture.objects.filter(title__contains=query) or Picture.objects.filter(artist__contains=query) or Picture.objects.filter(url__contains=query)
+    context = {'picture': search_results, 'query': query}
 
     return render(request, 'search.html', context)
 
@@ -38,7 +39,7 @@ def create_comment(request):
     params = request.POST
     picture_id = params['picture']
     picture = Picture.objects.get(pk=picture_id) 
-    
+
     comment = Comment() 
     comment.name = params['name']
     comment.message = params['message']
